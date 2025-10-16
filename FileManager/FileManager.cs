55955f15ecfcd3e79df2d30f2064abca61d27148
Phantomchik563 <103052076+Phantomchik563@users.noreply.github.com>
2025-10-16 22:21:11 +0300

    Добавил к решению проект FileManager, для создания библиотеки файловых функций.

--- FileManager/FileManager.cs
+++ FileManager/FileManager.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace FileManager
 {
-    public class Class1
+    public static class Zip
     {
+        
     }
 }
---
+++ Folder_Guard/FileManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folder_Guard
+{
+    internal class FileManager
+    {
+        public static void printXX()
+        {
+            Console.WriteLine("fff");
+        }
+    }
+}
