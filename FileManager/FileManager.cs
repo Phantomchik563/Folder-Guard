100379fddbde8ea2a3e2f25bbc170441e1e9b7f6
Phantomchik563 <103052076+Phantomchik563@users.noreply.github.com>
2025-10-17 21:04:00 +0300

    Создал метод getVaults() просматривающий хранилища Начал делать метод createVault(name, password)

--- FileManager/FileManager.cs
+++ FileManager/FileManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,12 +11,32 @@ namespace FileManager
 {
     public static class Vault
     {
-        //public static (List<string> vaults) getVaults()
-        //{
-        //    if (Directory.Exists("Vaults"))
-        //    {
-                
-        //    }
-        //}
+        public static List<string> getVaults() // Метод, возвращающий список хранилищ
+        {
+            List<string> vaults = new List<string>();
+            if (Directory.Exists("Vaults") == false) Directory.CreateDirectory(@"Vaults\");
+
+            string[] vaultFolders = Directory.GetDirectories(@"Vaults\");
+            foreach (string dir in vaultFolders)
+            {
+                vaults.Add(dir);
+            }
+
+            return vaults;
+        }
+        public static int createVault(string vaultName, string vaultPassword) // Метод, создающий хранилище (Возвратные коды: 0 - всё ок; 1 - в названии недопустимые символы; 2 - такое имя уже есть в списке)
+        {
+            char[] exeptionChars = {'\\', '|', '/', '?', '\'', '*', ':', '<', '>'}; // Список недопустимых символов
+            foreach(char c in vaultName)
+            {
+                if (exeptionChars.Contains(c)) return 1; // Проверка на недопустимые символы
+            }
+            List<string> vaults = getVaults();
+            if (vaults.Contains(vaultName)) return 2; // Проверка на повторяющиеся хранилища
+
+            //Позже реализую создание файлов...
+
+            return 0;
+        }
     }
 }
