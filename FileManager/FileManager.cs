166d8c27b0bfe9751119da701f5f1fde467eb568
Ivan Kovalev <ivango36372@gmail.com>
2025-10-17 16:28:22 +0300

    Начал делать метод просмотра хранилищ

--- FileManager/FileManager.cs
+++ FileManager/FileManager.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FileManager
 {
-    public static class Zip
+    public static class Vault
     {
-        
+        //public static (List<string> vaults) getVaults()
+        //{
+        //    if (Directory.Exists("Vaults"))
+        //    {
+                
+        //    }
+        //}
     }
 }
