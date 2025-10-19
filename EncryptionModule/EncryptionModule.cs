46358bd3b836da816aab729df07f53a3694c68c9
TxGSMFPC <dmitrijvoronin238@gmail.com>
2025-10-19 13:06:46 +0300

    Небольшие изменения Добавил возврат кодов, при правильной и неправильной отработке программы:     0 - Успех     1, 2 и 3 - Ошибки

--- EncryptionModule/EncryptionModule.cs
+++ EncryptionModule/EncryptionModule.cs
@@ -6,54 +6,70 @@ namespace EncryptionModule
 {
     public class Encryption
     {
-        public void EncryptFile(string inputPath, string outputPath, string metaSalt, string metaIterations, string password)
+        public int EncryptFile(string inputPath, string outputPath, string metaSalt, string metaIterations, string password)
         {
-            //=============================== Обработка входных данных ===============================
-            byte[] salt = Convert.FromBase64String(metaSalt);
-            byte[] key; //Массив для ключа
-            int iterations = int.Parse(metaIterations);
-            byte[] hmacKey;
-            string fileName = Path.GetFileName(inputPath);
-            byte[] fileNameBytes = System.Text.Encoding.UTF8.GetBytes(fileName);
-
-            //=============================== Создание ключей =========================================
-            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256))
+            try
             {
-                key = pbkdf2.GetBytes(32); // AES-256 ключ
-                hmacKey = pbkdf2.GetBytes(32); // HMAC-ключ
-            }
-            //Rfc2898DeriveBytes - класс для реализации PBKDF2 
-            //HashAlgorithmName.SHA256 - использование алгоритма SHA-256
+                //=============================== Обработка входных данных ===============================
+                byte[] salt = Convert.FromBase64String(metaSalt);
+                byte[] key; //Массив для ключа
+                int iterations = int.Parse(metaIterations);
+                byte[] hmacKey;
+                string fileName = Path.GetFileName(inputPath);
+                byte[] fileNameBytes = System.Text.Encoding.UTF8.GetBytes(fileName);
 
-            //=============================== Создание AES-объекта ===================================
-            using (Aes aes = Aes.Create()) //aes - переменная типа Aes
-            {
-                aes.KeySize = 256;
-                aes.Key = key;
-                aes.GenerateIV(); //случайный IV
-                aes.Mode = CipherMode.CBC; //Режим шифрования (В CBC каждый блок зависит от предыдущего)
-                aes.Padding = PaddingMode.PKCS7; //Увеличение размера блока до 16 байт
-
-                //=========================== Шифрование =============================================
-                using (FileStream fsInput = new FileStream(inputPath, FileMode.Open, FileAccess.Read))
-                using (FileStream fsOutput = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                //=============================== Создание ключей =========================================
+                using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256))
                 {
-                    fsOutput.WriteByte((byte)fileNameBytes.Length); //1 байт для длины имени
-                    fsOutput.Write(fileNameBytes, 0, fileNameBytes.Length); //Имя файла
-                    fsOutput.Write(aes.IV, 0, aes.IV.Length); //Ввод IV в начало файла
-                    using (CryptoStream cryptoStream = new CryptoStream(fsOutput, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    key = pbkdf2.GetBytes(32); // AES-256 ключ
+                    hmacKey = pbkdf2.GetBytes(32); // HMAC-ключ
+                }
+                //Rfc2898DeriveBytes - класс для реализации PBKDF2 
+                //HashAlgorithmName.SHA256 - использование алгоритма SHA-256
+
+                //=============================== Создание AES-объекта ===================================
+                using (Aes aes = Aes.Create()) //aes - переменная типа Aes
+                {
+                    aes.KeySize = 256;
+                    aes.Key = key;
+                    aes.GenerateIV(); //случайный IV
+                    aes.Mode = CipherMode.CBC; //Режим шифрования (В CBC каждый блок зависит от предыдущего)
+                    aes.Padding = PaddingMode.PKCS7; //Увеличение размера блока до 16 байт
+
+                    //=========================== Шифрование =============================================
+                    using (FileStream fsInput = new FileStream(inputPath, FileMode.Open, FileAccess.Read))
+                    using (FileStream fsOutput = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
                     {
-                        fsInput.CopyTo(cryptoStream); //Копирование содержимого файла в поток шифрования cryptoStream
+                        fsOutput.WriteByte((byte)fileNameBytes.Length); //1 байт для длины имени
+                        fsOutput.Write(fileNameBytes, 0, fileNameBytes.Length); //Имя файла
+                        fsOutput.Write(aes.IV, 0, aes.IV.Length); //Ввод IV в начало файла
+                        using (CryptoStream cryptoStream = new CryptoStream(fsOutput, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            fsInput.CopyTo(cryptoStream); //Копирование содержимого файла в поток шифрования cryptoStream
+                        }
                     }
                 }
+                //=========================== Добавление HMAC =========================================
+                using (var hmac = new HMACSHA256(hmacKey))
+                using (FileStream fsEncrypted = new FileStream(outputPath, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    byte[] hash = hmac.ComputeHash(fsEncrypted); //Чтение файла
+                    fsEncrypted.Seek(0, SeekOrigin.End); //Переход в конец файла
+                    fsEncrypted.Write(hash, 0, hash.Length); //запись HMAC в конец файла
+                }
+                return 0; //Файл успешно зашифрован
             }
-            //=========================== Добавление HMAC =========================================
-            using (var hmac = new HMACSHA256(hmacKey))
-            using (FileStream fsEncrypted = new FileStream(outputPath, FileMode.Open, FileAccess.ReadWrite))
+            catch (IOException)
             {
-                byte[] hash = hmac.ComputeHash(fsEncrypted); //Чтение файла
-                fsEncrypted.Seek(0, SeekOrigin.End); //Переход в конец файла
-                fsEncrypted.Write(hash, 0, hash.Length); //запись HMAC в конец файла
+                return 1; //Ошибка чтения/записи
+            }
+            catch (CryptographicException)
+            {
+                return 2; //Ошибка шифрования файла
+            }
+            catch (Exception)
+            {
+                return 3; //Неизвестная ошибка
             }
         }
         public static string GetSalt()
