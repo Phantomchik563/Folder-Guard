fa35ecbf2c63596bd746103869ced44a91e774a6
Takoyama <criper.ciller2000@gmail.com>
2025-10-22 11:35:53 +0300

    Сделал скрытое отобрадение пароля.

--- Folder_Guard/Form1.Designer.cs
+++ Folder_Guard/Form1.Designer.cs
@@ -40,18 +40,21 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(17, 50);
+            this.textBox1.Location = new System.Drawing.Point(23, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(308, 31);
+            this.textBox1.Size = new System.Drawing.Size(409, 37);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(17, 150);
+            this.textBox2.Location = new System.Drawing.Point(23, 185);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(308, 31);
+            this.textBox2.Size = new System.Drawing.Size(409, 37);
             this.textBox2.TabIndex = 1;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // label4
             // 
@@ -59,9 +62,10 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Location = new System.Drawing.Point(16, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(313, 25);
+            this.label4.Size = new System.Drawing.Size(391, 31);
             this.label4.TabIndex = 2;
             this.label4.Text = "Введите название хранилища";
             // 
@@ -71,9 +75,10 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Location = new System.Drawing.Point(16, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(331, 25);
+            this.label3.Size = new System.Drawing.Size(415, 31);
             this.label3.TabIndex = 3;
             this.label3.Text = "Введите пароль для хранилища";
             // 
@@ -84,9 +89,10 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Niagara Solid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button3.Location = new System.Drawing.Point(17, 195);
+            this.button3.Location = new System.Drawing.Point(23, 240);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(101, 29);
+            this.button3.Size = new System.Drawing.Size(135, 36);
             this.button3.TabIndex = 14;
             this.button3.Text = "Отмена";
             this.button3.UseVisualStyleBackColor = false;
@@ -99,9 +105,10 @@
             this.buttonDelStorage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDelStorage.Font = new System.Drawing.Font("Niagara Solid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonDelStorage.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.buttonDelStorage.Location = new System.Drawing.Point(224, 194);
+            this.buttonDelStorage.Location = new System.Drawing.Point(299, 239);
+            this.buttonDelStorage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonDelStorage.Name = "buttonDelStorage";
-            this.buttonDelStorage.Size = new System.Drawing.Size(101, 29);
+            this.buttonDelStorage.Size = new System.Drawing.Size(135, 36);
             this.buttonDelStorage.TabIndex = 15;
             this.buttonDelStorage.Text = "Ввести";
             this.buttonDelStorage.UseVisualStyleBackColor = false;
@@ -109,9 +116,9 @@
             // 
             // FormCreateStorage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 227);
+            this.ClientSize = new System.Drawing.Size(457, 279);
             this.Controls.Add(this.buttonDelStorage);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
@@ -120,6 +127,7 @@
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormCreateStorage";
             this.Text = "FolderGuard";
             this.ResumeLayout(false);
--- Folder_Guard/FormCode.Designer.cs
+++ Folder_Guard/FormCode.Designer.cs
@@ -42,19 +42,22 @@ namespace Folder_Guard
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Niagara Solid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(326, 23);
+            this.label1.Size = new System.Drawing.Size(423, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Введите код для открытия хранилища";
+            this.label1.Text = "Введите пароль для открытия хранилища";
             // 
             // textBoxCode
             // 
             this.textBoxCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxCode.Location = new System.Drawing.Point(16, 60);
+            this.textBoxCode.Location = new System.Drawing.Point(21, 74);
+            this.textBoxCode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxCode.Name = "textBoxCode";
-            this.textBoxCode.Size = new System.Drawing.Size(321, 29);
+            this.textBoxCode.Size = new System.Drawing.Size(427, 34);
             this.textBoxCode.TabIndex = 1;
+            this.textBoxCode.UseSystemPasswordChar = true;
             this.textBoxCode.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // buttonAddStorage
@@ -64,9 +67,10 @@ namespace Folder_Guard
             this.buttonAddStorage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAddStorage.Font = new System.Drawing.Font("Niagara Solid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonAddStorage.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.buttonAddStorage.Location = new System.Drawing.Point(16, 123);
+            this.buttonAddStorage.Location = new System.Drawing.Point(21, 151);
+            this.buttonAddStorage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonAddStorage.Name = "buttonAddStorage";
-            this.buttonAddStorage.Size = new System.Drawing.Size(101, 29);
+            this.buttonAddStorage.Size = new System.Drawing.Size(135, 36);
             this.buttonAddStorage.TabIndex = 15;
             this.buttonAddStorage.Text = "Отмена";
             this.buttonAddStorage.UseVisualStyleBackColor = false;
@@ -79,9 +83,10 @@ namespace Folder_Guard
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Niagara Solid", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(237, 123);
+            this.button1.Location = new System.Drawing.Point(316, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.Size = new System.Drawing.Size(135, 36);
             this.button1.TabIndex = 16;
             this.button1.Text = "Ввести";
             this.button1.UseVisualStyleBackColor = false;
@@ -89,15 +94,16 @@ namespace Folder_Guard
             // 
             // FormCode
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 177);
+            this.ClientSize = new System.Drawing.Size(464, 218);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonAddStorage);
             this.Controls.Add(this.textBoxCode);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormCode";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FolderGuard";
